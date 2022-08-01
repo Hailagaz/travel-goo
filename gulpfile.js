67c60fa0e573af3adde8d0d995e0065f59b398a4
Hailagaz <omentielvo.lumenn@gmail.com>
2022-08-01 15:00:08 +0300

    Practice

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,20 @@
-function defaultTask(cb) {
-	// place code for your default task here
-	cb();
-  }
-  
-  exports.default = defaultTask
+const { src, dest, series } = require('gulp');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass')(require('sass'));
+
+function js() {
+	return src('js/*.js')
+		.pipe(babel())
+		.pipe(uglify())
+		.pipe(dest('output/'));
+}
+
+function css() {
+	return src('scss/*.scss')
+		.pipe(sass())
+		.pipe(dest('output/'));
+}
+
+exports.default = series(js, css);
+
