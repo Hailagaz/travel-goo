6030b15ef2a2762d14cfbbdb7d3a9da2a4adb8d7
Hailagaz <omentielvo.lumenn@gmail.com>
2022-08-01 15:19:56 +0300

    Add concat to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const { src, dest, series } = require('gulp');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
 
 function js() {
 	return src('js/*.js')
@@ -13,8 +14,10 @@ function js() {
 function css() {
 	return src('scss/*.scss')
 		.pipe(sass())
+		.pipe(concat('index.css'))
 		.pipe(dest('output/'));
 }
+
 
 exports.default = series(js, css);
 
