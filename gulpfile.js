c94a9143fc1717264f4be359751aeb72e6a17940
Hailagaz <omentielvo.lumenn@gmail.com>
2022-08-17 16:44:49 +0300

    Fix gulpfile, add header

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
+//const gulp = require('gulp');
 const { src, dest, series } = require('gulp');
+
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const sass = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 //const watch = require('gulp');
-const livereload = require('gulp-livereload');
-const gulp = require('gulp');
+//const livereload = require('gulp-livereload');
+
 
 function js() {
 	return src('js/*.js')
@@ -18,14 +20,15 @@ function css() {
 	return src('scss/*.scss')
 		.pipe(sass())
 		.pipe(concat('index.css'))
-		.pipe(dest('output/'))
-		.pipe(livereload({ start: true }));
+		.pipe(dest('output/'));
+		//.pipe(livereload({ start: true }));
 }
 
-gulp.task('watch', function () {
-	livereload.listen();
-	gulp.watch('./scss/*.scss', ['scss']);
-});
+
+// gulp.task('watch', function () {
+// 	livereload.listen();
+// 	gulp.watch('scss/*.scss', ['scss']);
+// });
 
 exports.default = series(js, css);
 
