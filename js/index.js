86998324943b9b8d3062e1adcefea08a69237550
Hailagaz <omentielvo.lumenn@gmail.com>
2022-09-12 13:54:46 +0300

    Add swiper logic

--- js/index.js
+++ js/index.js
@@ -3,5 +3,20 @@ document.addEventListener("DOMContentLoaded", () => {
         document.querySelector(window.location.hash).scrollIntoView();
     }
 
-	
+	fetch("js/testimonials.json")
+		.then((response) => response.json())
+		.then((data) => {
+			let slides = '';
+
+			for (let testimonial of data) {
+				slides += ``; 
+			}
+
+			document.querySelector('.swiper').innerHTML = ``;
+
+			
+		})
+		.catch((error) => {
+			document.querySelector('.swiper').innerHTML = '<p>Sorry, we have some problems. We will fix them soon.</p>';
+		});
 });
