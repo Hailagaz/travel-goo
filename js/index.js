6f1a2f966c64d997fefa03a2ae48b41d27d0a124
Hailagaz <omentielvo.lumenn@gmail.com>
2022-09-12 13:59:37 +0300

    Add swiper logic

--- js/index.js
+++ js/index.js
@@ -14,7 +14,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
 			document.querySelector('.swiper').innerHTML = ``;
 
-			
+			const swiper = new Swiper('.swiper', {
+				loop: true,
+				slidesPerView: 1.5,
+				spaceBetween: 30,
+
+				pagination: {
+					el: '.swiper-pagination',
+					clickable: true,
+				},
+				navigation: {
+					nextEl: '.swiper-button-next',
+					prevEl: '.swiper-button-prev',
+				},
+			});
 		})
 		.catch((error) => {
 			document.querySelector('.swiper').innerHTML = '<p>Sorry, we have some problems. We will fix them soon.</p>';
